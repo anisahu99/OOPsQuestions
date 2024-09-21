f0f6b3f8df461a4ff3e466def50a0ce2f07dcb6d
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2024-09-21 13:31:04 +0530

    DIfferent ways of defining function

---
+++ Functionways.cpp
@@ -0,0 +1,23 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define odd(n) (n&1)
+#define even(n) !(n&1)
+
+
+
+void function(int x){
+    cout<<"function declaration then definition"<<endl;
+}
+
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    void function(int x);
+    function(2);
+    return 0;
+}
